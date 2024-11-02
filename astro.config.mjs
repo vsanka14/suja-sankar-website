602f00050d2d0f6c469d1afce6330af90cdf720b
Vasisht Shankar <vsanka14@asu.edu>
2024-11-02 17:21:10 -0500

    1 more

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,15 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
+  markdown: {
+    remarkPlugins: [],
+    shikiConfig: {
+      // Choose your preferred theme (optional)
+      theme: "github-light",
+      // Languages to enable syntax highlighting for
+      langs: ["javascript", "python", "html", "css"],
+      // Enable word wrap (optional)
+      wrap: true,
+    },
+  },
 });
