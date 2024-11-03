8dc2dbb89e695e1641189b007fa68e527d2419e6
Vasisht Shankar <vsanka14@asu.edu>
2024-11-03 11:06:52 -0600

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   integrations: [tailwind()],
@@ -19,11 +22,11 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@": path.resolve("./src"),
-        "@components": path.resolve("./src/components"),
-        "@layouts": path.resolve("./src/layouts"),
-        "@styles": path.resolve("./src/styles"),
-        "@content": path.resolve("./src/content"),
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@layouts": path.resolve(__dirname, "./src/layouts"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+        "@content": path.resolve(__dirname, "./src/content"),
       },
     },
   },
