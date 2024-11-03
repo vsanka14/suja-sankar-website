d52f112d5b79138f1d86043fa9bd8c19de933339
Vasisht Shankar <vsanka14@asu.edu>
2024-11-03 10:54:41 -0600

    Use absolute paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import path from "path";
 
 export default defineConfig({
   integrations: [tailwind()],
@@ -13,6 +14,17 @@ export default defineConfig({
       langs: ["javascript", "python", "html", "css"],
       // Enable word wrap (optional)
       wrap: true,
+    },
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+        "@components": path.resolve("./src/components"),
+        "@layouts": path.resolve("./src/layouts"),
+        "@styles": path.resolve("./src/styles"),
+        "@content": path.resolve("./src/content"),
+      },
     },
   },
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,1 @@
 /// <reference path="../.astro/types.d.ts" />
-
-declare module "*.astro" {
-  const content: any;
-  export default content;
-}
